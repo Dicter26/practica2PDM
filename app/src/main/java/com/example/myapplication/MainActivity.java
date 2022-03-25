d016bf9312e466c7c1d73a2822898b80f9fd9bba
DerekAlmanza <derek.almanza@ciencias.unam.mx>
2022-03-24 21:18:57 -0600

    Resolviendo imprevistos

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,3 @@
-/**
- * @author Derek Almanza -
- * <a href="Derek.Almanza@ciencias.unam.mx" >Derek.Almanza@ciencias.unam.mx</a>
- *
- */
-
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
